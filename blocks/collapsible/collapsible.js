f63c4f25e7a995ca0316e4ac8a236810dd487642
LeoBahrmann <104844214+LeoBahrmann@users.noreply.github.com>
2022-07-13 13:52:51 +0200

    feat (faq): inroduce collapsible block

---
+++ blocks/collapsible/collapsible.js
@@ -0,0 +1,14 @@
+export default function decorate(block) {
+    Array.from(block.querySelectorAll("h2")).forEach((h2,i) => {
+        const radio = document.createElement("input");
+        radio.type = "radio";
+        radio.name = "callapsible";
+        radio.id = "h" + i;
+        h2.after(radio);
+        const label = document.createElement("label");
+        label.htmlFor = radio.id;
+        label.append(h2);
+        radio.after(label);
+    });
+    
+}
