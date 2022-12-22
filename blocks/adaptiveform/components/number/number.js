e2c842b8f9c32418e397cc08ca1289c35bcc9980
Pareesh <paregupt@Pareeshs-MacBook-Pro.local>
2022-12-23 01:58:42 +0530

    Correcting minor styling

--- blocks/adaptiveform/components/number/number.js
+++ blocks/adaptiveform/components/number/number.js
@@ -21,8 +21,27 @@ export class NumberInput extends DefaultField {
         }
     }
 
+    // @todo customize using better way
+    customize() {
+        let state = this.model?.getState();
+
+        if(state && state.displayFormat) {
+            let widget = getWidget(this.element);
+            let div = document.createElement("div");
+            let span = document.createElement("span");
+            span.classList.add("number-format");
+            span.textContent = state.displayFormat.replace("{}", "");
+
+            div.append(span);
+
+            this.element.insertBefore(div, widget);
+            div.append(widget);
+        }
+    }
+
     render() {
-        this.element = builder?.default?.renderField(this.model, this.blockName)
+        this.element = builder?.default?.renderField(this.model, this.blockName);
+        this.customize();
         this.block.appendChild(this.element);
         this.addListener();
         subscribe(this.model, this.element, {value : this._updateValue});
