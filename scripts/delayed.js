119ac0aad52a56bac4eae8f47dc2c66f733dd467
Satya Deep Maheshwari <satyam@adobe.com>
2023-02-03 16:23:32 +0530

    Removing GTM

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,20 +5,6 @@ import { sampleRUM } from './scripts.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-(function gtm(w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({
-    'gtm.start': new Date().getTime(),
-    event: 'gtm.js',
-  });
-  const f = d.getElementsByTagName(s)[0];
-  const j = d.createElement(s);
-  const dl = l !== 'dataLayer' ? `&l=${l}` : '';
-  j.async = true;
-  j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
-  f.parentNode.insertBefore(j, f);
-}(window, document, 'script', 'dataLayer', 'GTM-JRNHH3P'));
-
 (function adobeotm() {
   const adobeotmscript = document.createElement('script');
   adobeotmscript.setAttribute('src', 'https://assets.adobedtm.com/6422e0f550a2/017d80491d7e/launch-1e8527b948f6-development.min.js');
