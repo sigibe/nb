24a617e290b6d69236aed2bfd6b2e07793c0ea80
piyushjindal <piyushjindal@adobe.com>
2023-03-22 17:37:35 +0530

    Issue #55 Minor Change

--- blocks/login/login.js
+++ blocks/login/login.js
@@ -10,12 +10,21 @@ export default async function decorate(block) {
 
     const loginHeader = document.createElement('div');
     loginHeader.classList.add('login-header');
-    const loginHeaderPara = block.querySelector('div p');
-    loginHeader.appendChild(loginHeaderPara);
+    const overLayClose = block.querySelector('div p');
+    loginHeader.appendChild(overLayClose);
 
     const loginMain = block.querySelector('.login-main');
     const loginParent = block.querySelector('div');
     loginParent.insertBefore(loginHeader, loginMain);
+
+    if (overLayClose) {
+      overLayClose.addEventListener('click', () => {
+        const loginEle = document.querySelector('.login-overlay');
+        const mainEle = document.querySelector('body');
+        loginEle.classList.remove('modal');
+        mainEle.classList.remove('overflow-hidden');
+      });
+    }
 
     makeLinksRelative(block);
     decorateIcons(block);
