a5b734a30208f6329288a65bc6421d8afc597980
vdua <vdua@adobe.com>
2023-04-18 09:20:09 +0530

    loading base after dependencies

--- blocks/header/nav-utils.js
+++ blocks/header/nav-utils.js
@@ -12,8 +12,8 @@ const REPLACE_SCRIPTS = new Map([
 
 function appendStyles() {
   [
-    '/blocks/header/nb-clientlibs/styles/clientlibs-base.css',
     '/blocks/header/nb-clientlibs/styles/clientlibs-dependencies.css',
+    '/blocks/header/nb-clientlibs/styles/clientlibs-base.css',
     '/blocks/header/nb-clientlibs/styles/clientlibs-site.css',
   ].forEach((item) => {
     const style = document.createElement('link');
