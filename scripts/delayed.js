097a2af975341766088d5047e8fc2dcf43045819
hparwani2 <himanshu.parwani8@gmail.com>
2023-05-04 11:07:21 +0530

    Fixing screen fliker issue

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -177,8 +177,5 @@ function createDigitalData() {
 
 // add more delayed functionality here
 (function adobeotm() {
-  const adobeotmscript = document.createElement('script');
   createDigitalData();
-  adobeotmscript.setAttribute('src', 'https://assets.adobedtm.com/6422e0f550a2/017d80491d7e/launch-1e8527b948f6-development.min.js');
-  document.head.append(adobeotmscript);
 }());
